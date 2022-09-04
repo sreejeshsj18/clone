5da8b2e944e25a24dc78b3062d0c543c4ed4ba31
sreejeshsj18 <106619703+sreejeshsj18@users.noreply.github.com>
2022-09-04 14:02:54 +0530

    Delete src directory

--- src/Constants/Constants.js
+++
@@ -1,3 +0,0 @@
-export const imageUrl='https://image.tmdb.org/t/p/original'
-export const baseUrl='https://api.themoviedb.org/3'
-export const API_KEY='cfd76b1397a24556c66f7adf0a5d1424'
--- src/axios/axios.js
+++
@@ -1,8 +0,0 @@
-import { baseUrl } from "../Constants/Constants";
-import axios from "axios";
-const instance = axios.create({
-    baseURL: baseUrl,
-    
-  });
-  
-  export default instance
